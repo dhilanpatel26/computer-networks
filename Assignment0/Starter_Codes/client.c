e1568961100858d5499891eeb0432f134d418470
dhilanpatel26 <dpatel93@jhu.edu>
2025-02-06 10:38:30 -0500

    Handling binary in client

--- Assignment0/Starter_Codes/client.c
+++ Assignment0/Starter_Codes/client.c
@@ -56,20 +56,21 @@ int client(char *server_ip, char *server_port) {
 }
 
 void chat_with_server(int sockfd) {
-  int len, bytes_sent, total_bytes_sent;
+  size_t bytes_read, bytes_sent, total_bytes_sent;
   char buffer[SEND_BUFFER_SIZE];
 
   fflush(stdin);
-  while (fgets(buffer, SEND_BUFFER_SIZE, stdin)) { // reads size - 1 chars/bytes
-    len = strlen(buffer);
+  while ((bytes_read = read(STDERR_FILENO, buffer, SEND_BUFFER_SIZE)) > 0) { // reads size - 1 chars/bytes
     total_bytes_sent = 0;
-    while (total_bytes_sent < len) {
-      bytes_sent = send(sockfd, buffer + total_bytes_sent, len - total_bytes_sent, 0);
+    while (total_bytes_sent < bytes_read) {
+      bytes_sent = send(sockfd, buffer + total_bytes_sent, 
+                      bytes_read - total_bytes_sent, 0);
       total_bytes_sent += bytes_sent;
       // printf("Bytes sent: %d\n", bytes_sent);
     }
   }
-  if (!feof(stdin)) {
+  if (bytes_read < 0) { // did not reach eof
+    perror("read error");
     close(sockfd);
     exit(1);
   }

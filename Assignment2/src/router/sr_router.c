f70aa7c729c4f1d8d821f803cda09fcb22b5c96d
Dhilan Patel <dhilantanpatel@gmail.com>
2025-03-23 22:12:27 -0400

    Taking care of all IP packet cases

--- Assignment2/src/router/sr_router.c
+++ Assignment2/src/router/sr_router.c
@@ -127,35 +127,52 @@ void sr_handle_ip_packet(struct sr_instance* sr,
   }
 }
 
-void sr_handle_icmp_packet(struct sr_instance* sr,
+void sr_handle_ip_packet(struct sr_instance* sr,
   uint8_t* packet/* lent */,
   unsigned int len,
   char* interface/* lent */) {
 
-  struct sr_icmp_hdr *icmp_hdr = (struct sr_icmp_hdr *)(packet + sizeof(struct sr_ethernet_hdr) + sizeof(struct sr_ip_hdr));
-
-  // Create response packet
-  uint8_t *new_packet = (uint8_t *)malloc(len);
-  memcpy(new_packet, packet, len);
-
-  if (icmp_hdr->icmp_type == 8) {
-    printf("ICMP echo request\n");
-    sr_handle_icmp_echo_request(sr, packet, new_packet, len, interface);
-  } else {
-    printf("Unknown ICMP packet\n");
-    free(new_packet);  // Free if not handled
+  // Validate packet length
+  if (len < sizeof(struct sr_ethernet_hdr) + sizeof(struct sr_ip_hdr)) {
     return;
   }
 
-  // Send packet and free memory
-  struct sr_ip_hdr *ip_hdr = (struct sr_ip_hdr *)(packet + sizeof(struct sr_ethernet_hdr));
-  struct sr_rt *rt = sr_get_longest_prefix_match(sr, ip_hdr->ip_src);
-  if (rt) {
-    struct sr_if *out_iface = sr_get_interface(sr, rt->interface);
-    sr_send_packet(sr, new_packet, len, out_iface->name);
+  // Verify checksum
+  struct sr_ip_hdr* ip_hdr = (struct sr_ip_hdr*)(packet + sizeof(struct sr_ethernet_hdr));
+  uint16_t old_sum = ip_hdr->ip_sum;
+  ip_hdr->ip_sum = 0;
+  uint16_t calculated_sum = cksum(ip_hdr, sizeof(struct sr_ip_hdr));
+  ip_hdr->ip_sum = old_sum;
+  if (old_sum != calculated_sum) {
+    return;
   }
 
-  free(new_packet);
+  // Is this packet for one of our interfaces?
+  int for_us = 0;
+  struct sr_if* iface = sr->if_list;
+  while (iface) { // search through interfaces list
+    if (iface->ip == ip_hdr->ip_dst) {
+        for_us = 1;
+        break;
+    }
+    iface = iface->next;
+  }
+
+  if (for_us) {
+    // Packet is for us - handle based on protocol
+    if (ip_hdr->ip_p == ip_protocol_icmp) {
+        printf("ICMP packet for us\n");
+        sr_handle_icmp_packet(sr, packet, len, interface);
+    } else {
+        // Generate ICMP port unreachable for TCP/UDP
+        printf("TCP/UDP packet for us - generating port unreachable\n");
+        sr_send_icmp_port_unreachable(sr, packet, interface);
+    }
+  } else {
+    // Need to forward this packet
+    printf("Forwarding IP packet\n");
+    sr_forward_packet(sr, packet, len, interface);
+  }
 }
 
 void sr_handle_icmp_echo_request(struct sr_instance* sr,
